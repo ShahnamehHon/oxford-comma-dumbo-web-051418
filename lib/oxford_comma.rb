8d1c15b8915516b88f4b16c032fdaa197309c1a5
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-09 17:41:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
-end
+  if array.size == 1 
+    return array.join
+  elsif array.size == 2 
+    return array.join(" and ")
+  else
+    oxford = array.pop
+    oxford_comma = array.join(", ").concat(", and #{oxford}")
+  end
