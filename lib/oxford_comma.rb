76c7f89e20c39326d6f45e73d9de66ecd468b171
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-09 17:59:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.size == 1 
-    return array.join
+    array.join
   elsif array.size == 2 
-    return array.join(" and ")
+    array.join(" and ")
   else
     conjunction = array.pop
     oxford_comma = array.join(", ").concat(", and #{conjunction}")
